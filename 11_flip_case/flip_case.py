a11ba5e20ac58540f3777dc895b1b77012ae00a5
Artemio Guerrero <aguerrero2113@gmail.com>
2022-04-27 15:48:59 -0700

    modified and added solutions

--- 11_flip_case/flip_case.py
+++ 11_flip_case/flip_case.py
@@ -1,13 +1,12 @@
 def flip_case(phrase, to_swap):
-    """Flip [to_swap] case each time it appears in phrase.
+    to_swap = to_swap.lower()
+    out = ""
 
-        >>> flip_case('Aaaahhh', 'a')
-        'aAAAhhh'
-
-        >>> flip_case('Aaaahhh', 'A')
-        'aAAAhhh'
-
-        >>> flip_case('Aaaahhh', 'h')
-        'AaaaHHH'
-
-    """
+    for letter in phrase:
+        if letter.lower() == to_swap:
+            letter = letter.swapcase()
+        out += letter
+    return out
+print((flip_case("Aaaahh","a")))
+print((flip_case("Aaaahh","A")))
+print((flip_case("Aaaahh","h")))
--- 35_two_oldest_ages/two_oldest_ages.py
+++ 35_two_oldest_ages/two_oldest_ages.py
@@ -13,7 +13,9 @@ def two_oldest_ages(ages):
         >>> two_oldest_ages([1, 5, 5, 2])
         (2, 5)
     """
-
+    uniq_ages = set(ages)
+    oldest = sorted(uniq_ages)[-2:]
+    return tuple(oldest)
     # NOTE: don't worry about an optimized runtime here; it's fine if
     # you have a runtime worse than O(n)
 
